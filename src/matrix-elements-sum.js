102eb246c2458d3621b5f6f6299c314f2755359c
SergeyFedoruk <7282289@tut.by>
2023-04-17 20:29:05 +0300

    feat: add  Monday 17.04.2023 20_28

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -16,9 +16,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function getMatrixElementsSum( matrix ) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let sum = 0;
+
+  for (let i=0; i<matrix.length; i++){
+    for (let j=0; j<matrix[i].length; j++){
+      if (matrix[i][j] === 0) {
+        for (let k=i+1; k<matrix.length; k++){
+          matrix[k][j] = 0;
+        }
+      }
+    }
+  }
+
+  for (let l=0; l<matrix.length; l++){
+    for (let m=0; m<matrix[l].length; m++){
+      sum =sum + matrix[l][m]
+    }
+  }
+
+  return sum;
 }
 
 module.exports = {
--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,10 +23,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
+function minesweeper( matrix ) {
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-}
+
+  /* var neaarestElements = ([i, j]) => [
+     [i - 1, j - 1],
+     [i - 1, j],
+     [i - 1, j + 1],
+     [i, j - 1],
+     [i, j + 1],
+     [i + 1, j - 1],
+     [i + 1, j],
+     [i + 1, j + 1],
+   ];
+   var hintsMatrix = matrix.map((r) => r.map((_) => 0));
+
+   for (var i = 0; i < matrix.length; i++) {
+     for (var j = 0; j < matrix[i].length; j++) {
+       if (matrix[i][j]) {
+         var myAd = neaarestElements([i, j]);
+         for (var k = 0; k < 8; k++) {
+           if (
+               myAd[k][0] >= 0 &&
+               myAd[k][1] >= 0 &&
+               myAd[k][0] < matrix.length &&
+               myAd[k][1] < matrix[i].length
+           ) {
+             hintsMatrix[myAd[k][0]][myAd[k][1]]++;
+           }
+         }
+       }
+     }
+   }
+
+   return hintsMatrix;
+   */
+
+ }
 
 module.exports = {
   minesweeper
